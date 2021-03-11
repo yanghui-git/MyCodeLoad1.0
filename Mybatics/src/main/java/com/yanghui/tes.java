bb86d641cc215c29a7ccce420b237fe1706529f5
hui.yang <15868172375@163.com>
2021-03-11 15:11:22 +0800

    LOmbok @Builder 注解 神奇

--- Mybatics/src/main/java/com/yanghui/tes.java
+++
@@ -1,7 +0,0 @@
-package com.yanghui;
-
-public class tes {
-    public static void main(String[] args){
-        System.out.println("666");
-    }
-}
---
+++ SpringEasy/src/main/java/com/yanghui/lombock/StudentLombok.java
@@ -0,0 +1,22 @@
+package com.yanghui.lombock;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class StudentLombok {
+
+    String name;
+
+    int age;
+
+    String address;
+
+    Long phone;
+
+    public static void main(String[] args) {
+        StudentLombok studentLombok = StudentLombok.builder().name("haha").age(100).build();
+        System.out.println(studentLombok);
+    }
+}
